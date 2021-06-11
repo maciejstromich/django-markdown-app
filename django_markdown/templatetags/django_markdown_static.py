7038847c69c5246b2269edc6b4cbeb9826d21bcb
Maciej Strömich <maciej.stromich@gmail.com>
2021-06-11 17:27:34 +0200

    Making Django-markdown-app only Django3 compatible

--- django_markdown/templatetags/django_markdown_static.py
+++ django_markdown/templatetags/django_markdown_static.py
@@ -3,10 +3,7 @@ from django.template import Library
 
 register = Library()
 
-if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
-    from django.contrib.staticfiles.templatetags.staticfiles import static as _static  # noqa
-else:
-    from django.templatetags.static import static as _static
+from django.templatetags.static import static as _static
 
 
 @register.simple_tag
